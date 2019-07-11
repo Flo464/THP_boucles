c1e4feb7b8576198d35a2caa75226b3b9b227683
Arnaudboy <r.boyer895@laposte.net>
2019-07-11 16:00:56 +0200

    ajout fichier stairway jeux fonctionnel sans stat

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,24 +1,33 @@
 
-def jeu 
-marche = 0
+def jeu
+mar = 0
 
-	while marche <= 10
-		print "Appuies sur Entréé pour lancer de dé !"
-		a = gets.chomp
-		dé = rand(1..6)
-			if dé == 5 || dé == 6 
-				puts "Tu montes une marche"
-				marche = marche +1
-			
-				end
-			elsif dé == 1 
-				puts "tu descends d'une marche"
-				marche = marche - 1 			
-			else 
-				puts "Rien ne se passe"
-			end 
+	while mar < 10
+		print "lancez le dé \n"
+a = gets.chomp
+de = rand(1..6)
+	if de == 1
+		print "VOUS RECULEZ D'UNE MARCHE \n"
+		if mar == 0
+			mar = mar
+		else
+		mar = mar - 1
+	end
+		print "VOUS ETES SUR LA MARCHE #{mar} \n"
+
+	elsif de == 2 || de == 3 || de == 4
+		print "VOUS RESTEZ SUR LA MARCHE \n"
+		mar = mar 
+		print "VOUS ETES SUR LA MARCHE #{mar} \n"
+	elsif de	== 5 || de == 6
+		print "VOUS AVANCEZ D'UNE MARCHE \n"
+		mar = mar + 1
+		print "VOUS ETES SUR LA MARCHE #{mar} \n"
+
+	end
+	if mar == 10
+		print "VOUS AVEZ GAGNE"		
 	end
 end
-
-
-jeu 
+end
+jeu
