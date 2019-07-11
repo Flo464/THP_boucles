39cfd03e59cdc2b72df8ddc97a7faf45e3e2cd15
Arnaudboy <r.boyer895@laposte.net>
2019-07-11 17:20:28 +0200

    fin de l'exo stairway

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,42 +1,47 @@
-
 def jeu
 mar = 0
 jet = 0
-vic = 0
 	while mar < 10
 		print "lancez le dé \n"
-a = gets.chomp
 de = rand(1..6)
-	if de == 1
-		print "VOUS RECULEZ D'UNE MARCHE \n"
-		if mar == 0
+		if de == 1
+			print "VOUS RECULEZ D'UNE MARCHE \n"
+			if mar == 0
+				print "VOUS ETES SUR LA MARCHE #{mar} \n"
+				mar = mar
+				jet = jet + 1
+			else
+				print "VOUS ETES SUR LA MARCHE #{mar} \n"
+				mar = mar - 1
+				jet = jet + 1
+			end
+		elsif de == 2 || de == 3 || de == 4
+			print "VOUS RESTEZ SUR LA MARCHE \n"
+			mar = mar 
 			print "VOUS ETES SUR LA MARCHE #{mar} \n"
-			mar = mar
 			jet = jet + 1
-		else
+		elsif de	== 5 || de == 6
+			print "VOUS AVANCEZ D'UNE MARCHE \n"
+			mar = mar + 1
 			print "VOUS ETES SUR LA MARCHE #{mar} \n"
-			mar = mar - 1
 			jet = jet + 1
-	end
-	elsif de == 2 || de == 3 || de == 4
-		print "VOUS RESTEZ SUR LA MARCHE \n"
-		mar = mar 
-		print "VOUS ETES SUR LA MARCHE #{mar} \n"
-		jet = jet + 1
-	elsif de	== 5 || de == 6
-		print "VOUS AVANCEZ D'UNE MARCHE \n"
-		mar = mar + 1
-		print "VOUS ETES SUR LA MARCHE #{mar} \n"
-		jet = jet + 1
 
+		end
+		if mar == 10
+			jet = jet + 1
+			print "VOUS AVEZ GAGNE EN #{jet} jets\n"	
+		end
 	end
-	if mar == 10
-		print "VOUS AVEZ GAGNE"	
-		jet = jet + 1
-		vic = vic + 1
-		print jet
-		print vic	
+	return jet		
+end
+
+def moyenne
+	jet = jeu
+	tab = 0
+	100.times do
+		jeu
+		tab = tab + jet
 	end
+	print "MOYENNE #{tab/100}\n"
 end
-end
-jeu
+moyenne
