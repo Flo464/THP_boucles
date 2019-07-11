9accac3832e489a17e664ea5a1edf717d86c33cf
Arnaudboy <r.boyer895@laposte.net>
2019-07-11 16:10:38 +0200

    push du fichier stairway

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,7 +1,8 @@
 
 def jeu
 mar = 0
-
+jet = 0
+vic = 0
 	while mar < 10
 		print "lancez le dé \n"
 a = gets.chomp
@@ -9,24 +10,32 @@ de = rand(1..6)
 	if de == 1
 		print "VOUS RECULEZ D'UNE MARCHE \n"
 		if mar == 0
+			print "VOUS ETES SUR LA MARCHE #{mar} \n"
 			mar = mar
+			jet = jet + 1
 		else
-		mar = mar - 1
+			print "VOUS ETES SUR LA MARCHE #{mar} \n"
+			mar = mar - 1
+			jet = jet + 1
 	end
-		print "VOUS ETES SUR LA MARCHE #{mar} \n"
-
 	elsif de == 2 || de == 3 || de == 4
 		print "VOUS RESTEZ SUR LA MARCHE \n"
 		mar = mar 
 		print "VOUS ETES SUR LA MARCHE #{mar} \n"
+		jet = jet + 1
 	elsif de	== 5 || de == 6
 		print "VOUS AVANCEZ D'UNE MARCHE \n"
 		mar = mar + 1
 		print "VOUS ETES SUR LA MARCHE #{mar} \n"
+		jet = jet + 1
 
 	end
 	if mar == 10
-		print "VOUS AVEZ GAGNE"		
+		print "VOUS AVEZ GAGNE"	
+		jet = jet + 1
+		vic = vic + 1
+		print jet
+		print vic	
 	end
 end
 end
